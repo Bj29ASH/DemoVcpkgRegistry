39ee9a4e17dd8681402df9c368dc21615d92d0a6
akash <akash@launchtrax.com>
2024-09-11 11:15:36 +0530

    Replaced geographiclib with one from official vcpkg repo

--- ports/geographiclib/portfile.cmake
+++ ports/geographiclib/portfile.cmake
@@ -1,17 +1,21 @@
-vcpkg_from_github(
-  OUT_SOURCE_PATH SOURCE_PATH
-  REPO geographiclib/geographiclib
-  REF v1.52
-  SHA512 e2cb0702172116a03a9ad7297a8e67c4503995480252613aaadc6ebae98744f270882b307d25678e15bfb1053637b38a0afa610471a869756a19f5f92b6bfe31
-  HEAD_REF main
-)
+vcpkg_from_sourceforge(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO geographiclib
+    REF distrib-C++
+    FILENAME "GeographicLib-${VERSION}.tar.gz"
+    SHA512 31b6019784d3379572f973e6bd99957cd977baeaec55a7702340e50285321158646b3a87ea96111b3f14ee08ea50bc3a1b2eb2c51641995e8f289518cc23f618
+    )
 
 vcpkg_check_features(
-  OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-  FEATURES
-  "tools" TOOLS
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        "tools" TOOLS
 )
 
+# GeographicLib's CMakeLists.txt allows the installation directories for
+# all the components to be set independently.  A "false" value, e.g., an
+# empty string or OFF (-DBINDIR=OFF), indicates that the corresponding
+# component should not be installed.
 if(TOOLS)
     set(TOOL_OPTION "-DBINDIR=tools/${PORT}")
 else()
@@ -19,13 +23,13 @@ else()
 endif()
 
 vcpkg_cmake_configure(
-  SOURCE_PATH "${SOURCE_PATH}"
-  OPTIONS ${TOOL_OPTION}
-  "-DCMAKEDIR=share/${PORT}"
-  -DDOCDIR=OFF
-  -DEXAMPLEDIR=OFF
-  -DMANDIR=OFF
-  -DSBINDIR=OFF
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS ${TOOL_OPTION}
+    "-DCMAKEDIR=share/${PORT}"
+    -DDOCDIR=OFF
+    -DEXAMPLEDIR=OFF
+    -DMANDIR=OFF
+    -DSBINDIR=OFF
 )
 
 vcpkg_cmake_install()
@@ -46,5 +50,6 @@ file(INSTALL "${SOURCE_PATH}/LICENSE.txt"
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
     RENAME copyright)
 
+# Install usage
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage"
     "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" @ONLY)
